5e32013c3469f0c60daf948eef15cb764b9fa78b
Viktor-MP <viktormelikparsadanyan@gmail.com>
2024-02-24 22:17:45 +0400

    socket testing

--- FrontEnd/src/chatProject/socket.js
+++ FrontEnd/src/chatProject/socket.js
@@ -1,5 +1,27 @@
+// import WebSocket from 'ws'
 
 
-// const socket = new WS("wss://javascript.info/article/websocket/demo/hello")
+const socket_chanal = (url) => {
+    const ws = new WebSocket(url, 'protocolOne');
 
-// console.log(Websocket)
+    ws.onopen = (e) => {
+      console.log(e,"opend")
+          ws.send({
+      "messages": [
+        {
+          "role": "user",
+          "content": "բառև"
+        }
+      ]
+    })
+    }
+    ws.onclose = (e) => console.log(e, "onclose")
+    ws.onerror = (e) => console.log("error", e)
+    ws.onmessage = (e) => console.log(e)
+    console.log(ws)
+
+}
+
+
+
+export { socket_chanal }
