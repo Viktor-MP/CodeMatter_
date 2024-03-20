65b6dcfd5d24e6bd8f4d8c9b4b007846ab6935ef
Viktor-MP <viktormelikparsadanyan@gmail.com>
2024-03-20 23:03:28 +0400

    check

--- ai_chat/src/Components/PersonalChat/ChatComponents/Message/ChatMessage.tsx
+++ ai_chat/src/Components/PersonalChat/ChatComponents/Message/ChatMessage.tsx
@@ -1,71 +1,70 @@
-import { FC, useState, CSSProperties } from "react"
-import { chatMessageType, pose } from "./chatMessageType"
+import { FC, useState, CSSProperties, MouseEvent, FocusEvent } from "react";
+import {
+  chatMessageType,
+  toolsObjType,
+  messageStyle,
+  elPosition,
+  toolsObj,
+  pose,
+  toolsDrowType,
+} from "./chatMessageType";
 
-import "./ChatMessage.scss"
+import "./ChatMessage.scss";
 
-const elPosition = {
-    left: 0 ,
-    top : 0 ,
-}
+const ChatMessage: FC<chatMessageType> = ({ className, chat }) => {
+  const toolTypes: string[] = ["_hiden_tools", "_show_tools"];
 
+  const [toolsView, setToolsView] = useState(toolTypes[0]);
+  const [elPose, setElPose] = useState<pose>(elPosition);
 
-const ChatMessage: FC <chatMessageType> = ({  className, chat  })  => {
-    const toolTypes : string[] = ["_hiden_tools", "_show_tools"]
+  const rcStyle: CSSProperties = {
+    left: elPose.left,
+    top: elPose.top,
+  };
 
-    const [tools,setTools] = useState(toolTypes[0])
-    const [elPose, setElPose] = useState<pose>(elPosition)
-    
-    
-    const rc: CSSProperties = {
-        left: elPose.left,
-        top: elPose.top
-     
+  const openExtraTools = (
+    e: MouseEvent<HTMLDivElement> | FocusEvent<HTMLDivElement>
+  ) => {
+    e.preventDefault();
+    console.log(toolsObj);
+
+    if ("pageX" in e && toolsView === toolTypes[0]) {
+      const target = e.currentTarget as HTMLDivElement;
+      const mouseX = e.pageX - target.getBoundingClientRect().left;
+      const mouseY = e.pageY - target.getBoundingClientRect().top;
+      setElPose({
+        left: mouseX,
+        top: mouseY,
+      });
+        setToolsView(toolTypes[1]);
+    } else {
+        setToolsView(toolTypes[0]);
     }
-    // console.log(rc)
-    
-    const openExtraTools = (e:
-        React.MouseEvent<HTMLDivElement> |
-        React.FocusEvent<HTMLDivElement>
-        ) => {
+  };
 
-        e.preventDefault();
-        
-        // e.stopImmediatePropagation();
-        
-    
-        if ("pageX" in e && tools === toolTypes[0]) {
-
-            const target =  e.currentTarget as HTMLDivElement;
-            const mouseX = e.pageX - target.getBoundingClientRect().left;
-            const mouseY = e.pageY - target.getBoundingClientRect().top;            
-            setElPose({
-                left: mouseX,
-                top: mouseY
-            })
-            setTools(toolTypes[1])
-        } else {
-            setTools(toolTypes[0])
-        }
-
-    }
+  const DrowTools: FC<toolsDrowType>= ({tools}) => {
+    console.log(tools);
     
     return (
-            <div  
-                onContextMenu = {openExtraTools }
-                onBlur = { openExtraTools } 
-                className={`${className} _message`}
-                tabIndex={0}  >
+            <ul  className={` ${toolsView}  defTool`} style={rcStyle}>
+                {tools.map(tool => <li key={tool.id}  onClick={tool.action}>{tool.name}</li>)}
+            </ul>
+            );
+  };
+  console.log(chat)
+  return (
+    <div
+      onContextMenu={openExtraTools}
+      onBlur={openExtraTools}
+      className={`${className} _message`}
+      style={messageStyle}
+      tabIndex={0}
+    >
+      <pre> {chat.content} </pre>
 
-                <pre> {chat.content} </pre>
+      {<DrowTools tools={toolsObj} />}
+    </div>
+  );
+};
 
-                <div   className = { `${tools}   defTool `}  style  = {rc} >
-                    <p>Delete</p>
-                    <p>Edit</p>
-                    <p>Copy text</p>
-                </div>
-
-            </div>
-            )
-}
-
-export default ChatMessage
+export default ChatMessage;
--- ai_chat/src/Components/PersonalChat/ChatComponents/Message/chatMessageType.ts
+++ ai_chat/src/Components/PersonalChat/ChatComponents/Message/chatMessageType.ts
@@ -1,3 +1,4 @@
+import { CSSProperties } from "react";
 import { myMessage } from "../UserChat/typesUserChat";
 
 
@@ -6,10 +7,55 @@ export interface pose {
     top: number;
 }
 
+export interface toolsObjType {
+    "id" : number;
+    "name" :string;
+    "usage" : boolean;
+    "action" : (e: React.MouseEvent<HTMLLIElement>) => void;
+}
+
 export interface chatMessageType {
     chat: myMessage
     className: string
 }
 
+export interface toolsDrowType {
+    tools: toolsObjType[]
+}
 
 
+
+const elPosition = {
+    left: 0 ,
+    top : 0 ,
+}
+
+
+const messageStyle: CSSProperties = {
+    boxShadow: "0px 0px 30px -10px rgba(0,0,0,0.79)"
+}
+
+
+const toolsObj: toolsObjType[] = [
+    {
+        "id" : 0,
+        "name" : "Delete",
+        "usage" : true,
+        "action" : (e: any) => {console.log(e)}
+    },
+    {
+        "id" : 1,
+        "name" : "Eddet",
+        "usage" : false,
+        "action" : (e: any) => {console.log(e)}
+    },
+    {
+        "id" : 2,
+        "name" : "Copy text",
+        "usage" : true,
+        "action" : (e: any) => {console.log(e)}
+    }
+  ]
+
+
+export { elPosition, messageStyle,toolsObj }
