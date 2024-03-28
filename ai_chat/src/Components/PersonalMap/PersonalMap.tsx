ecd69056eabff0883fbb8bd15fd52064fd235225
Viktor-MP <viktormelikparsadanyan@gmail.com>
2024-03-29 01:14:53 +0400

    nav bar

--- ai_chat/src/Components/PersonalMap/PersonalMap.tsx
+++ ai_chat/src/Components/PersonalMap/PersonalMap.tsx
@@ -4,17 +4,20 @@ import { FC } from "react"
 import { PersonMapType, DeveloperLevel } from "./typesPersonMap"
 import fetchData from "../../AxiosRequest/AxiosRequest"
 
+import { Sling as Hamburger } from "hamburger-react"
 
+import classNames from "classnames"
 import "./PersonalMap.scss"
 import "../../App.css"
+
 
 
 const PersonalMap: FC <PersonMapType> = ({className}) => {
 const [mapData,setMapData] = useState<DeveloperLevel[]>()
 const [width, setWidth] = useState<number>(window.innerWidth)
+let state = width < 1050? false : true
+const [burger, setBurger] = useState<boolean>(state)
 
-const [burger, setBurger] = useState<boolean>(false)
- 
  
 useEffect(() => {
     fetchData("./sources/map.json")
@@ -23,31 +26,27 @@ useEffect(() => {
  }, [])
 
 
-//  useEffect(() => {
-//   console.log(burger)
-// }, [burger])
-// console.log(width, burger)
 
 
  useEffect(() => {
 
   const handleResize = () => {
     setWidth(window.innerWidth);
-    console.log(width, burger)
-    if (width < 950) {
-      setBurger(true)
-    } else {
-      setBurger(false)
-    }
+  
   };
 
+  console.log(width, burger)
+  if (width < 1050) {
+    setBurger(false)
+  } else {
+    setBurger(true)
+  }
   window.addEventListener("resize", handleResize);
 
   return () => {
     window.removeEventListener("resize", handleResize);
   };
 }, [width]);
-
 
 
 
@@ -58,9 +57,12 @@ const mapDataHandler = (data: DeveloperLevel[])  => {
     </div>)
 }
 
-
   return (
-    <section className={className}>
+    <section className={classNames(className, {
+      ["hide"]: !burger
+    })}>
+      <Hamburger  toggled={burger} toggle={setBurger}  direction="left" duration={0.8}  color="#1cea1c"  easing="ease-in"/>
+
       <div className="_mapContainer_">
         <div className="_mapGide_">
           <h2>Personal education map</h2>
